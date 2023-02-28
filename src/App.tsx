3d23929201eafc569ff56d79d168727829f8527a
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2023-02-28 15:11:28 +0100

    fix: remove unused class

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="max-w-5xl bg-slate-200 border-x-2 border-slate-600 h-screen overflow-auto mx-auto px-8 py-8">
-      <h1 className="h">Find movies</h1>
+      <h1>Find movies</h1>
       <MovieSearch></MovieSearch>
     </div>
   );
