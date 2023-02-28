e4e6bb407884a20957c7d7775efe3c7058008356
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2023-02-28 15:14:12 +0100

    fix: remove unused count

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { MovieSearch } from './MovieSearch';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="max-w-5xl bg-slate-200 border-x-2 border-slate-600 h-screen overflow-auto mx-auto px-8 py-8">
       <h1>Find movies</h1>
