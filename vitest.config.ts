471fdf0db65ab610a401b0a6a7afe4c280c67466
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2023-05-31 14:57:19 +0200

    fix: add happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 // vite.config.ts
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-  },
+    environment: 'happy-dom'
+  }
 });
