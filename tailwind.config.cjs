8281e8f5574607f8e2bcce72d3a37cd839e0aceb
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2023-06-07 19:29:34 +0200

    added eslint as recommended

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./index.html", "./src/**/*.tsx"],
+  content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("daisyui")],
-}
+  // eslint-disable-next-line global-require
+  plugins: [require('daisyui')]
+};
